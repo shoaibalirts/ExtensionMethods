0f95ba00d3406017c8f823478194089f54e37473
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-13 11:07:58 +0200

    1. create the actual class without extension method, 2. create static class along with static method with the first parameter with this keyword, 3. call the extension method

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace ExtensionMethods
+﻿using System;
+using ClassLibrary1;
+
+namespace ExtensionMethods
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Product product = new Product();
+            product.ProductCost = 100;
+            product.DiscountPercentage = 10;
+            Console.WriteLine(product.GetDiscount());
+
+
         }
     }
 }
